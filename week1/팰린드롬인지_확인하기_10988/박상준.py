caf6eec57d486bc6534a783e6240893589860272
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-10-19 22:06:47 +0900

    feat:  일곱난쟁이 문제 해결
    Author: 박상준

---
+++ week1/팰린드롬인지_확인하기_10988/박상준.py
@@ -0,0 +1,16 @@
+"""
+ *packageName    : 
+ * fileName       : 박상준
+ * author         : ipeac
+ * date           : 2022-10-19
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-10-19        ipeac       최초 생성
+ """
+n = list(input())
+print(1) if n == n[::-1] else print(0)
+
+
+

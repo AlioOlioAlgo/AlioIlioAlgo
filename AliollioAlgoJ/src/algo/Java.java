9b5fddcccd8bb8e20dba20f91c1784d95ff4bc35
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-10-20 21:57:16 +0900

    feat: 자바파일 서브모듈로 생성
    Author: 박상준

---
+++ AliollioAlgoJ/src/algo/Java.java
@@ -0,0 +1,25 @@
+/**
+ packageName    : algo
+ fileName       : Java
+ author         : ipeac
+ date           : 2022-10-20
+ description    :
+ ===========================================================
+ DATE              AUTHOR             NOTE
+ -----------------------------------------------------------
+ 2022-10-20        ipeac       최초 생성 */
+package algo;
+
+/**
+ *packageName    : algo
+ * fileName       : Java
+ * author         : ipeac
+ * date           : 2022-10-20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-10-20        ipeac       최초 생성
+ */
+public class Java {
+}

cf0eb37292e413b39084e7d20d2bb3f1f33d55e5
LeeJongbok <s2feeling@naver.com>
2022-10-20 22:52:35 +0900

    Create 이종복.java

---
+++ week1/java/트럭 주차_2979/이종복.java
@@ -0,0 +1,43 @@
+import java.util.*; 
+
+public class Main {
+    
+    public static int[] arr;
+    
+    public static void main(String args[]) {
+      Main T = new Main();
+      Scanner sc = new Scanner(System.in);
+      int sum = 0; 
+      
+      int A = sc.nextInt();
+      int B = sc.nextInt();
+      int C = sc.nextInt();
+      
+      int[] arr = new int[110];
+      
+      for(int i=0; i<3; i++){
+          int start = sc.nextInt();
+          int end = sc.nextInt(); 
+          
+          for(int j=start; j<end; j++){
+              arr[j] += 1;
+          }
+      }
+      
+      for(int i=1; i<=100; i++){
+          if(arr[i] == 0){
+              continue;
+          }else if(arr[i] == 1){
+              sum += A;
+          }else if(arr[i] == 2){
+              sum += 2*B;
+          }else if(arr[i] == 3){
+              sum += 3*C; 
+          }
+      }
+      
+      System.out.println(sum);
+      
+      
+    }
+}

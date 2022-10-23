5010778ff5c5152c3dd6ffee3cd80ade73212797
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-10-23 20:42:59 +0900

    feat: java 폴더 생성
    Author: 박상준

--- week2/python/쿼드트리_1992/박상준.py
+++ week2/python/쿼드트리_1992/박상준.py
@@ -25,8 +25,15 @@ def quad_tree(x, y, n):
     color = graph[x][y]
     for i in range(x, x + n):
         for j in range(y, y + n):
-            # if color
-            pass
+            if color != graph[i][j]:  # 범위안에 한개라도 다른 경우.. 4분면으로 나눠서 다시 검색한다.
+                result.append("(")
+                quad_tree(x, y, n // 2)
+                quad_tree(x, y + n // 2, n // 2)
+                quad_tree(x + n // 2, y, n // 2)
+                quad_tree(x + n // 2, y + n // 2, n // 2)
+                result.append(")")
+                return
+    result.append(color)  # 재귀 x 범위안의 모든 수가 같으면 color 숫자를 담는다.
 
 quad_tree(0, 0, n)
 print("".join(map(str, result)))

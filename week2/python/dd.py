4715d7102c32ae08d367d80ecbc6f717633dd1dc
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-10-25 21:08:37 +0900

    feat: 상준

---
+++ week2/python/dd.py
@@ -0,0 +1,13 @@
+"""
+ *packageName    : 
+ * fileName       : dd
+ * author         : ipeac
+ * date           : 2022-10-25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-10-25        ipeac       최초 생성
+ """
+num = 2
+print("even" if num % 2 == 0 else "odd")

b9a59d13ebf01901e4a3feeaf0006c6625d62bdb
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-10-25 13:18:44 +0900

    feat: 상준

--- week2/python/균형잡힌 세상_4949/박상준.py
+++ week2/python/균형잡힌 세상_4949/박상준.py
@@ -9,3 +9,35 @@
  * -----------------------------------------------------------
  * 2022-10-24        ipeac       최초 생성
  """
+
+while True:
+    sentence = list(map(str, input()))
+    if len(sentence) == 1 and sentence[-1] == '.':
+        exit()
+    sentence = sentence[:-1]
+    pivot = ''
+    stack = []
+    for idx, sence in enumerate(sentence):
+        if sence == '(':
+            stack.append('(')
+            pivot = '('
+        elif sence == ')':
+            stack.append(')')
+            pivot = ')'
+        
+        if sence == '[':
+            stack.append('[')
+            pivot = '['
+        elif sence == ']':
+            stack.append(']')
+            pivot = ']'
+        if len(stack) >= 2 and stack[-1] == ']' and stack[-2] == '[':
+            stack.pop()
+            stack.pop()
+        elif len(stack) >= 2 and stack[-1] == ')' and stack[-2] == '(':
+            stack.pop()
+            stack.pop()
+    if not stack:
+        print("yes")
+    else:
+        print('no')

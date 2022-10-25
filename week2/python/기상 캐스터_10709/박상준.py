cc0144a0d1196ac098b57c6349183b11aec9a666
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-10-25 09:13:26 +0900

    feat: 상준

--- week2/python/기상 캐스터_10709/박상준.py
+++ week2/python/기상 캐스터_10709/박상준.py
@@ -9,3 +9,29 @@
  * -----------------------------------------------------------
  * 2022-10-23        ipeac       최초 생성
  """
+h, w = map(int, input().split())
+sky = [
+    list(map(str, input()))
+    for _ in range(h)
+]
+# print(f"h,w = {h, w}")
+# print(f"sky = {sky}")
+
+# h, w = (3, 4)
+# sky = [['c', '.', '.', 'c'], ['.', '.', 'c', '.'], ['.', '.', '.', '.']]
+for sk in sky:
+    sky_chk = False
+    i = 0
+    for s in sk:
+        if s == 'c':
+            sky_chk = True
+        if not sky_chk:
+            print(-1, end=" ")
+        else:
+            if s == 'c':
+                print(0, end=" ")
+                i = 0
+            else:
+                i += 1
+                print(i, end=" ")
+    print()

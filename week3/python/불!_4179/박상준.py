69d92c0bb84b511e0fae9bbadc0b749a3e7997a3
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-10-29 16:35:24 +0900

    feat: 상준 + 실패 (75%)
    5 4
    ####
    #...
    #.##
    #.J#
    ####
    IMPOSSIBLE
    
    수정해야한다.
    
    불이 없는 경우를 생각을 안했음
    불이 없는 경우을 조건으로 추가함

--- week3/python/불!_4179/박상준.py
+++ week3/python/불!_4179/박상준.py
@@ -48,13 +48,13 @@ def bfs():
             if 0 <= nx < r and 0 <= ny < c and not visited_f[nx][ny] and miro[nx][ny] != '#':
                 fire_queue.append([nx, ny])
                 visited_f[nx][ny] = visited_f[x][y] + 1
-    # print(f"visited_f = {visited_f}")
+    print(f"visited_f = {visited_f}")
     while jihun_queue:
         x, y = jihun_queue.popleft()
         for xx, yy in zip(dx, dy):
             nx, ny = xx + x, yy + y
             if 0 <= nx < r and 0 <= ny < c:
-                if not visited_j[nx][ny] and miro[nx][ny] != '#' and visited_j[x][y] + 1 < visited_f[nx][ny]:
+                if not visited_j[nx][ny] and miro[nx][ny] != '#' and (visited_j[x][y] + 1 < visited_f[nx][ny] or not visited_f[nx][ny]):
                     jihun_queue.append([nx, ny])
                     visited_j[nx][ny] = visited_j[x][y] + 1
             # 그래프 밖으로 꾸역꾸역 기어나가는 경우 > 위험에서 벗어난거임

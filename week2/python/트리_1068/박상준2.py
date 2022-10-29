f29dd55c412912d00646c9e8befccdc3c4527332
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-10-29 23:39:42 +0900

    feat: 문제
    Author: 박상준

---
+++ week2/python/트리_1068/박상준2.py
@@ -0,0 +1,24 @@
+"""
+ *packageName    : 
+ * fileName       : 박상준2
+ * author         : ipeac
+ * date           : 2022-10-29
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-10-29        ipeac       최초 생성
+ """
+# n = int(input())
+# node = list(map(int, input().split()))
+# tree = [-1] * n
+# del_node = int(input())
+# print(f"n = {n}")
+# print(f"node = {node}")
+# print(f"tree = {tree}")
+# print(f"del_node = {del_node}")
+n = 5
+node = [-1, 0, 0, 1, 1]
+# 기본으로 -1 로 설정 후 부모 노드 설정
+tree = [-1, -1, -1, -1, -1]
+del_node = 2

e87e9cd537180bb7b0a3687bc5a462f715ef2b52
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-10-30 17:18:05 +0900

    feat: 문제
    Author: 박상준

---
+++ week3/python/부등호_2529/박상준.py
@@ -0,0 +1,19 @@
+"""
+ *packageName    : 
+ * fileName       : 박상준
+ * author         : ipeac
+ * date           : 2022-10-30
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-10-30        ipeac       최초 생성
+ """
+# k = int(input())
+# inequality_sign = list(map(str, input().split()))
+
+# print(f"k = {k}")
+# print(f"inequality_sign = {inequality_sign}")
+
+k = 2
+inequality_sign = ['<', '>']

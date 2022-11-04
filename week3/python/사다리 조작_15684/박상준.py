26698c141c5adf9e84eb27ccbf299c0eeda0e69e
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-11-04 18:17:20 +0900

    feat: 상준

--- week3/python/사다리 조작_15684/박상준.py
+++ week3/python/사다리 조작_15684/박상준.py
@@ -1,5 +1,5 @@
 """
- *packageName    : 
+ *packageName    :
  * fileName       : 박상준
  * author         : ipeac
  * date           : 2022-11-04
@@ -9,3 +9,5 @@
  * -----------------------------------------------------------
  * 2022-11-04        ipeac       최초 생성
  """
+n, m, h = map(int, input().split())
+print(f"n, m, h = {n, m, h}")

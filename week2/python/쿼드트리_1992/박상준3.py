33647101b85d2efb6e397bd2ecaa5c556b36842c
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-11-08 23:33:13 +0900

    feat: 문제
    Author: 박상준

--- week2/python/쿼드트리_1992/박상준3.py
+++ week2/python/쿼드트리_1992/박상준3.py
@@ -9,19 +9,28 @@
  * -----------------------------------------------------------
  * 2022-11-08        ipeac       최초 생성
  """
-n = 8
-# screen = [list(map(int, input())) for _ in range(n)]
+n = int(input())
+screen = [list(map(int, input())) for _ in range(n)]
 # print(f"screen = {screen}")
-screen = [[1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 1, 1], [1, 1, 1, 1, 0, 0, 1, 1]]
+# screen = [[1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0, 1, 1], [1, 1, 1, 1, 0, 0, 1, 1]]
+result = ''
 
-# 해당 구역안에서 같은 값만이 존재하지 않는다면
-def check(start, end, nv):
-    first_value = screen[start][end]
+def div(start, end, nv):
+    global result
+    first_color = screen[start][end]
     for i in range(start, start + nv):
         for j in range(end, end + nv):
-            if first_value != screen[i][j]:
-                return False
-    return True
+            if first_color != screen[i][j]:
+                div_value = nv // 2
+                result += '('
+                div(start, end, div_value)
+                div(start, end + div_value, div_value)
+                div(start + div_value, end, div_value)
+                div(start + div_value, end + div_value, div_value)
+                result += ')'
+                return
+    # 다 같은 경우 하나로 압축이 가능하기에 .
+    result += str(first_color)
 
-def div():
-    pass
+div(0, 0, len(screen[0]))
+print(result)

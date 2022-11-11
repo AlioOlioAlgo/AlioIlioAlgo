bc4af695ba812e5331a95971196693ece623ca53
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-11-11 20:08:05 +0900

    feat: 문제
    Author: 박상준

--- week3/python/괄호 추가하기_16637/박상준2.py
+++ week3/python/괄호 추가하기_16637/박상준2.py
@@ -1,5 +1,5 @@
 """
- *packageName    : 
+ *packageName    :
  * fileName       : 박상준2
  * author         : ipeac
  * date           : 2022-11-11
@@ -9,3 +9,57 @@
  * -----------------------------------------------------------
  * 2022-11-11        ipeac       최초 생성
  """
+import sys
+
+input = sys.stdin.readline
+
+result = -2 ** 31 - 1
+
+def calc(num, op):
+    while op:
+        oper = op.pop(0)
+        n1, n2 = num.pop(0), num.pop(0),
+        if oper == '+':
+            num.insert(0, n1 + n2)
+        elif oper == '-':
+            num.insert(0, n1 - n2)
+        elif oper == '*':
+            num.insert(0, n1 * n2)
+    return num[0]
+
+def solve(cnt, num, op):
+    global result
+    if cnt == n // 2 or len(num) == 1:  # 수식의 끝에 도달했거나, 남은 피연산자가 1개인 경우 종료
+        result = max(result, calc(num, op))
+        return
+    
+    solve(cnt + 1, num[:], op[:])  # 이번 연산자를 계산전에 미리 하나 뒤로 보내줌(모든 경우의 수 대비)
+    
+    try:
+        n1, n2 = num.pop(cnt), num.pop(cnt)
+        oper = op.pop(cnt)
+        if oper == '+':
+            num.insert(cnt, n1 + n2)
+        elif oper == '-':
+            num.insert(cnt, n1 - n2)
+        elif oper == '*':
+            num.insert(cnt, n1 * n2)
+        
+        solve(cnt + 1, num[:], op[:])
+    except:
+        0
+
+n = int(input())
+# n = 1
+expr = input()
+# expr = '1'
+num, op = [], []
+
+for i in range(n):
+    if i % 2 == 0:
+        num.append(int(expr[i]))
+    else:
+        op.append(expr[i])
+
+solve(0, num, op)
+print(result)

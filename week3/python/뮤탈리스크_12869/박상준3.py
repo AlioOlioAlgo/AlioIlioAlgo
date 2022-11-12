0f0570a85670bb43d54b4903f02e3b75db96f5c2
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-11-12 13:21:50 +0900

    feat: 상준

--- week3/python/뮤탈리스크_12869/박상준3.py
+++ week3/python/뮤탈리스크_12869/박상준3.py
@@ -11,24 +11,28 @@
  """
 from itertools import permutations
 
-n = 3
-scv = [4, 10, 12]
-scv = sorted(scv + [0] * (3 - n))  # 조로려~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-print(f"scv = {scv}")
+n = int(input())
+scv = list(map(int, input().split()))
+
 attack = [9, 3, 1]
-hp = [[[0] * 61 for _ in range(61)] for _ in range(61)]
+hp = [[[61] * 61 for _ in range(61)] for _ in range(61)]
 if len(scv) < 3:
     scv += [0] * (3 - n)
 
 def dfs(x, y, z):
-    if x == 0 and y == 0 and z == 0:
+    if x <= 0 and y <= 0 and z <= 0:
         return 0
-    if hp[x][y][z]:
-        return hp[x][y][z]
-        
-        pass
+    if x < 0:
+        x = 0
+    if y < 0:
+        y = 0
+    if z < 0:
+        z = 0
+    if hp[x][y][z] < 61: return hp[x][y][z]
+    for i, j, k in attack_case:
+        hp[x][y][z] = min(dfs(x - i, y - j, z - k) + 1, hp[x][y][z])
+    return hp[x][y][z]
 
-attack_case = list(permutations(attack, 3))  # scv에 순차적으로 데미지를 줄 순열
-print(f"attack_case = {attack_case}")
+attack_case = list(permutations(attack))  # scv에 순차적으로 데미지를 줄 순열
 
 print(dfs(scv[0], scv[1], scv[2]))

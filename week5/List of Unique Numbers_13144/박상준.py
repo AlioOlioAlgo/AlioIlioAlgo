c57e1d58bb84f2128b10a867e7c00fe58ef08408
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-11-23 22:58:17 +0900

    feat: 상준

---
+++ week5/List of Unique Numbers_13144/박상준.py
@@ -0,0 +1,16 @@
+"""
+ *packageName    :
+ * fileName       : 박상준
+ * author         : ipeac
+ * date           : 2022-11-23
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-11-23        ipeac       최초 생성
+ """
+
+n = int(input())
+# n = 100000
+# arr = [i for i in range(1, 100001)]
+arr = list(map(int, input().split()))

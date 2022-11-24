0d989477b02ac41d2d4bfc1a0bb839fa9e8696c9
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-11-24 21:46:33 +0900

    feat: 문제
    Author: 박상준

--- week5/List of Unique Numbers_13144/박상준.py
+++ week5/List of Unique Numbers_13144/박상준.py
@@ -9,17 +9,8 @@
  * -----------------------------------------------------------
  * 2022-11-23        ipeac       최초 생성
  """
+
 """
-5
-1 1 1 1 1
-
-5
-
-5
-1 2 3 4 5
-
-15
-
 5
 1 2 3 1 2
 
@@ -28,31 +19,18 @@
 
 n = int(input())
 # n = 100000
-# arr = [i for i in range(1, 100001)]
 arr = list(map(int, input().split()))
-start = 0
-end = 0
+# arr = [i for i in range(1, 100001)]
+cnt, end = 0, 0
 check = [0] * 100001
-ans = 0
-while start <= n - 1:
-    if end == n:
-        end = n - 1
-    # print("==========================================")
-    if not check[arr[end]]:  # 방문 x
-        # print(f"start = {start}")
-        # print(f"end = {end}")
-        # print(f"arr[end] = {arr[end]}")
-        ans += 1
+
+for start in range(len(arr)):
+    while end < n:
+        if check[arr[end]] == 1:
+            # 이미 방문한 경우는 안됨
+            break
         check[arr[end]] = 1
         end += 1
-    else:  # 방문 O
-        # print(f"start = {start}")
-        # print(f"end = {end}")
-        # print("방문 O")
-        check = [0] * 100001
-        start += 1
-        end = start
-        # print(f"start = {start}")
-        # print(f"end = {end}")
-
-print(ans)
+    cnt += end - start
+    check[arr[start]] = 0
+print(cnt)

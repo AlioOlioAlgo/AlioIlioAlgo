a7f68771a3af937f2751fd637f8168e070bdaf33
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-12-03 14:58:50 +0900

    feat: 상준

--- lecture/python/3주차/CandyLand.py
+++ lecture/python/3주차/CandyLand.py
@@ -9,23 +9,37 @@
  * -----------------------------------------------------------
  * 2022-12-03        ipeac       최초 생성
  """
-from functools import cache
+import math
+from itertools import product
 
-cnt = 0
+# cnt = 0
 n = int(input())
 
-arr = [0 for _ in range(n)]
-arr[1] = 1
+# arr = [0 for _ in range(n)]
+# arr[1] = 1
+# arr[2] = 2
+# arr[3] = 4
+# arr[4] = 8
+# arr[5] = 16
+# arr[6] = 32
+# arr[7] = 64
 
-print(f"arr = {arr}")
+# print(f"arr = {arr}")
 
-@cache
-def dp(n):
-    if n:
-        pass
+def make_distinct_arr(target):
+    tmp = [i for i in range(1, target + 1)]
+    cnt = 0
+    for i in range(1, target + 1):
+        for combi in product(tmp, repeat=i):
+            if sum(combi) == target:
+                cnt += 1
+                print(f"combi = {combi}")
+    print(f"cnt = {cnt}")
 
 if n == 0:
     print(0)
     exit()
-dp(1)
-print(cnt)
+print(int(math.pow(2, n - 1)))
+# make_distinct_arr(n)  # 테스트 케이스 체크를 위한 함수
+# dp(1)
+# print(cnt)

80b93f1d45d40274fc1933ce4421143889dbd934
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-12-07 17:29:53 +0900

    feat: 상준

---
+++ week5/연속합_1912/연속합.py
@@ -0,0 +1,14 @@
+"""
+
+ *packageName    :
+ * fileName       : 박상준
+ * author         : ipeac
+ * date           : 2022-12-07
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-12-07        ipeac       최초 생성
+ """
+n = int(input())
+number_list = list(map(int, input().split()))

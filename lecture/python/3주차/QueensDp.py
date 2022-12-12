c7bd29d38855539840964d970df8a385a52aba90
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-12-12 23:39:11 +0900

    feat: 문제
    Author: 박상준

--- lecture/python/3주차/QueensDp.py
+++ lecture/python/3주차/QueensDp.py
@@ -19,7 +19,6 @@ chess = [
 ]
 
 col = [0] * n
-
 for i in range(n):
     for j in range(n):
         # 해당 row 의 q 가 존재한다면 col을 기록한다.
@@ -36,29 +35,24 @@ def check(col, depth):
     # 열체크 + 대각 체크
     # 결국 모든 행을 전부 체크해야합니다. 뒷행도 존재하기에
     for i in range(depth):
-        if col[i] == col[depth] and abs(col[i] - col[depth]) == abs(i - depth):  # 겹치면 false 처리
+        if col[i] == col[depth] or abs(col[i] - col[depth]) == abs(i - depth):  # 겹치면 false 처리
             return False
     
     for i in range(depth + 1, n):
-        if col[i] == col[depth] and abs(col[i] - col[depth]) == abs(i - depth):  # 겹치면 false 처리
+        if col[i] == col[depth] or abs(col[i] - col[depth]) == abs(i - depth):  # 겹치면 false 처리
             return False
     return True
 
 def dfs(col, depth):
     print(f"col = {col}")
     print(f"depth = {depth}")
-    if depth == n:
-        print('true')
-        return True
-    else:
-        if col[depth]:
-            dfs(col, depth + 1)
-        for cc in range(1, n + 1):  # 열마다 조건 검사함 ㅋㅋ
-            # 일단 해당 행 depth 에 열 cc 에 값을 투입해서 검사진행
-            if not col[depth]:  # 값이 이미 둬진 경우는 해당행을 검사하지 않도록 해야한다.
-                col[depth] = cc
-                if check(col, depth):
-                    dfs(col, depth + 1)
-                col[depth] = 0
+    if check(col, depth):
+        if depth == n:
+            print('true')
+            return True
+        else:
+            for j in range(n):
+                col[depth + 1] = j
+                dfs(col, depth + 1)
 
-print(dfs(col, 0))
+dfs(col, 0)

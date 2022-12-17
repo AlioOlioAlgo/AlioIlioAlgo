172e1a42615b126cb61a9aa18a6f25a406d49acb
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2022-12-17 17:38:41 +0900

    feat: 상준

--- lecture/python/4주차/막대기(DP).py
+++ lecture/python/4주차/막대기(DP).py
@@ -15,9 +15,9 @@ stick_size = int(input())
 
 stick_arr = [i for i in range(1, stick_size + 1)]
 # print(f"stick_arr = {stick_arr}")
-r = {0: 0, 1: 1}
+r = {0: 0, 1: 1}  # dp 메모이제이션
 
-def dp(n, p, r):
+def dp(n, prices, r):
     # print("==========================================")
     # print(f"n = {n}")
     if n not in r.keys():
@@ -26,7 +26,7 @@ def dp(n, p, r):
         else:
             r[n] = -1
             for i in range(1, n + 1):
-                r[n] = max(r[n], p[i] + dp(n - i, p, r))  # 처음에 dp(1) = 1 반환 =>
+                r[n] = max(r[n], prices[i] + dp(n - i, prices, r))  # 처음에 dp(1) = 1 반환 =>
                 # print(f"p[i] = {p[i]}")
                 # print(f"dp(n - i, p, r) = {dp(n - i, p, r)}")
                 # print(f"r[n] = {r[n]}")
--- week6/python/보석상자_2792/박상준.py
+++ week6/python/보석상자_2792/박상준.py
@@ -9,27 +9,28 @@
  * -----------------------------------------------------------
  * 2022-12-14        ipeac       최초 생성
  """
-# import math, sys
-#
-# input = sys.stdin.readline
-# N, M = map(int, input().split())
-# jew = []
-# for _ in range(M):
-#     jew.append(int(input()))
-#
-# def binary_search(low, high):
-#     dab = 999999999
-#     while low <= high:
-#         tmp = 0
-#         mid = (low + high) // 2
-#         for i in jew:
-#             tmp += math.ceil(i / mid)
-#
-#         if tmp > N:
-#             low = mid + 1
-#         else:
-#             high = mid - 1
-#             dab = min(dab, mid)
-#     return dab
-#
-# print(binary_search(1, max(jew)))
+import math
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+jew = []
+for _ in range(M):
+    jew.append(int(input()))
+
+def binary_search(low, high):
+    dab = 999999999
+    while low <= high:
+        tmp = 0
+        mid = (low + high) // 2
+        for i in jew:
+            tmp += math.ceil(i / mid)
+        
+        if tmp > N:
+            low = mid + 1
+        else:
+            high = mid - 1
+            dab = min(dab, mid)
+    return dab
+
+print(binary_search(1, max(jew)))

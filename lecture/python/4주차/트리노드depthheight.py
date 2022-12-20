2094f61c06505f559d7c0b55bd7c6439748c752b
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-12-20 20:55:58 +0900

    feat: 문제
    Author: 박상준

--- lecture/python/4주차/트리노드depthheight.py
+++ lecture/python/4주차/트리노드depthheight.py
@@ -26,37 +26,40 @@ class TreeNode:
     def append_children(self, children):
         self.childrens.append(children)
 
-def make_tree():
-    root = TreeNode(None, 3, None)  # 루트
-    child1_depth1 = TreeNode(root, 1, None)
-    child2_depth1 = TreeNode(root, 7, None)
-    child3_depth1 = TreeNode(root, 4, None)
-    child1_depth2 = TreeNode(child1_depth1, 6, None)
-    child2_depth2 = TreeNode(child1_depth1, 6, None)
-    child3_depth2 = TreeNode(child2_depth2, 6, None)
-    child4_depth2 = TreeNode(child3_depth2, 6, None)
-    child5_depth2 = TreeNode(child3_depth2, 6, None)
-    # 0 자식의 자식 설정
-    child1_depth1.append_children(child1_depth2)
-    child1_depth1.append_children(child2_depth2)
-    # 1 자식의 자식 설정
-    child2_depth1.append_children(child3_depth2)
-    # 2 자식의 자식 설정
-    child3_depth1.append_children(child4_depth2)
-    child3_depth1.append_children(child5_depth2)
-    
-    print(root)
-    
-    return root
+root = TreeNode(None, 3, None)  # 루트
+child1_depth1 = TreeNode(root, 1, None)
+child2_depth1 = TreeNode(root, 7, None)
+child3_depth1 = TreeNode(root, 4, None)
+child1_depth2 = TreeNode(child1_depth1, 6, None)
+child2_depth2 = TreeNode(child1_depth1, 6, None)
+child3_depth2 = TreeNode(child2_depth1, 6, None)
+child4_depth2 = TreeNode(child3_depth1, 6, None)
+child5_depth2 = TreeNode(child3_depth1, 6, None)
+# 0 자식의 자식 설정
+child1_depth1.append_children(child1_depth2)
+child1_depth1.append_children(child2_depth2)
 
-make_tree()
+# 1 자식의 자식 설정
+child2_depth1.append_children(child3_depth2)
 
-def depth(n):
+# 2 자식의 자식 설정
+child3_depth1.append_children(child4_depth2)
+child3_depth1.append_children(child5_depth2)
+
+def depth(find_node, cnt):
+    # 부모는 무조건 하나임.
+    if find_node == root:
+        return cnt
+    return depth(find_node.parent, cnt + 1)
+
+def height(find_node, cnt):
+    return height(find_node)
     pass
 
-def height(n):
-    pass
 # 1. 재귀로 풀기 +
 # 2. 반복문 으로 풀기 +
 # depth()
 # height()
+#
+print(depth(child3_depth2, 0))
+print(height())

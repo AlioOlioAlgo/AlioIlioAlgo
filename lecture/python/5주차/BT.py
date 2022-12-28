d355daf1e6a96ce60f791b9d200e3e130332a5ba
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-12-28 21:13:55 +0900

    feat: 문제
    Author: 박상준

--- lecture/python/5주차/BT.py
+++ lecture/python/5주차/BT.py
@@ -11,11 +11,19 @@
  """
 
 class BinaryTree:
-    def __init__(self, left=None, right=None, parent=None, value=None):
-        self.left = left
-        self.right = right
-        self.value = value
-        self.parent = parent
+    class Node:
+        def __init__(self, left=None, right=None, parent=None, value=None):
+            self.left = left
+            self.right = right
+            self.value = value
+            self.parent = parent
+    
+    def __init__(self, root=None):
+        self.root = root
+        self.size = 1 if self.root else 0
+    
+    def length(self):
+        return self.size
     
     def print_preorder(self):
         # 전위 순회 값 출력 -> 리스트 형태로 값을 출력한다.
@@ -26,28 +34,36 @@ class BinaryTree:
             if target_tree.right:
                 dfs(target_tree.right)
         
-        dfs(self)
+        dfs(self.root)
     
     def add(self, e):  # 부모보다 작으면 왼쪽 크면 오른쪽
         def _add(node, e):
+            if node is None:
+                self.root = self.Node(None, None, None, e)
+                return
             if node.value < e:
                 if node.right:
                     _add(node.right, e)
                 else:
-                    node.right = BinaryTree(None, None, node, e)
+                    node.right = self.Node(None, None, node, e)
             if node.value > e:
                 if node.left:
                     _add(node.left, e)
                 else:
-                    node.left = BinaryTree(None, None, node, e)
+                    node.left = self.Node(None, None, node, e)
         
-        _add(self, e)
+        _add(self.root, e)
+        self.size += 1
 
-root = BinaryTree(None, None, None, 5)
+root = BinaryTree(None)
 root.add(7)
 root.add(4)
 root.add(3)
 # root.print_preorder()
 root.add(1)
 root.add(2)
+root.add(9)
+root.add(10)
+root.add(5)
+print(root.length())
 root.print_preorder()
--- lecture/python/6주차/[PCCP 모의고사 #1]유전 법칙.py
+++ lecture/python/6주차/[PCCP 모의고사 #1]유전 법칙.py
@@ -10,10 +10,38 @@
  * 2022-12-25        ipeac       최초 생성
  """
 import math
+import sys
+
+sys.setrecursionlimit(10 ** 6)
 
 def solution(queries):
     print("==========================================")
     answer = []
+    
+    def dfs(index_p):
+        if index_p == 0:
+            return
+        stack.append(index_p)
+        dfs((index_p - 1) // 4)
+    
+    def find_child(stack):  # [9  , 2]
+        pop_value = stack.pop()  # 2 -> 9
+        index_value = pop_value % 4
+        print(f"index_value = {index_value}")
+        
+        if index_value == 1:
+            answer.append("RR")
+            return
+        elif 2 <= index_value <= 3:  # 2 ->
+            if not stack:
+                answer.append("Rr")
+                print(f"answer = {answer}")
+                return
+            else:
+                find_child(stack)
+        
+        else:
+            answer.append("rr")
     
     for query in queries:
         # 인덱스는 1부터 시작합니다,.
@@ -22,32 +50,16 @@ def solution(queries):
             answer.append("Rr")
             continue
         
-        bigger_group_end_index = math.pow(4, generation - 2)
-        print(f"bigger_group_end_index = {bigger_group_end_index}")
-        
-        middle_group_end_index = 3 * (math.pow(4, generation - 2))
-        print(f"middle_group_end_index = {middle_group_end_index}")
-        
-        smaller_group_end_index = 4 * (math.pow(4, generation - 2))
-        print(f"smaller_group_end_index = {smaller_group_end_index}")
-        
-        if index <= bigger_group_end_index:
-            answer.append("RR")
-            pass
-        elif index <= middle_group_end_index:
-            index -= 1
-            print(f"middle_group_end_index = {middle_group_end_index}")
-            check = index % 4
-            if not check:
-                answer.append("RR")
-            elif 1 <= check <= 2:
-                answer.append("Rr")
-            else:
-                answer.append("rr")
-        else:
-            answer.append("rr")
-            pass
-    
+        prod = ((math.pow(4, generation - 1) - 1) // (4 - 1))
+        print(f"prod = {prod}")
+        index += int(prod - 1)
+        print(f"index = {index}")
+        # 인덱스  10
+        #  9로 치환
+        stack = []
+        dfs(index)
+        print(f"stack = {stack}")
+        find_child(stack)
     return answer
 
 print(solution([[3, 5]]))
@@ -56,8 +68,8 @@ print(solution([[3, 1], [2, 3], [3, 9]]))
 print(solution([[4, 26]]))
 print(solution([[1, 1]]))
 print(solution([[2, 1], [2, 2], [2, 3], [2, 4]]))
-# print(solution([[3, 4], [3, 6], [3, 7], [3, 8], [3, 9]]))
-# print(solution([[4, 20]]))
-# solution([[3, 8], [2, 2]])
-# solution([[3, 1], [2, 3], [3, 9]])
-# solution([[4, 26]])
+print(solution([[3, 4], [3, 6], [3, 7], [3, 8], [3, 9]]))
+print(solution([[4, 20]]))
+solution([[3, 8], [2, 2]])
+solution([[3, 1], [2, 3], [3, 9]])
+solution([[4, 26]])

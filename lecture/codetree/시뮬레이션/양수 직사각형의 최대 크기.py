4eca403e5078107992593af30955ba07cd02e326
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-12-30 12:11:21 +0900

    feat: 문제
    Author: 박상준

--- lecture/codetree/시뮬레이션/양수 직사각형의 최대 크기.py
+++ lecture/codetree/시뮬레이션/양수 직사각형의 최대 크기.py
@@ -14,12 +14,29 @@ graph = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
+
+def check_all_plus(start_row, start_col, end_row, end_col):
+    size = 0
+    for i in range(start_row, end_row + 1):
+        for j in range(start_col, end_col + 1):
+            if graph[i][j] <= 0:
+                return False, size
+            else:
+                size += 1
+    return True, size
+
+max_size = 0
 for i in range(n):
     for j in range(m):
         for row in range(i, n):
             for col in range(j, m):
-                print("==========================================")
-                print(f"i = {i}")
-                print(f"j = {j}")
-                print(f"row = {row}")
-                print(f"col = {col}")
+                # print("==========================================")
+                # print(f"i = {i}")
+                # print(f"j = {j}")
+                # print(f"row = {row}")
+                # print(f"col = {col}")
+                check, size_return = check_all_plus(i, j, row, col)
+                if check:
+                    max_size = max(max_size, size_return)
+
+print(max_size) if max_size else print(-1)

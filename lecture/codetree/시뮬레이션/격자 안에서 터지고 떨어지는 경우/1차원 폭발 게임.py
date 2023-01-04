c369b98db6f5c6f0373b0e64827a9cb36c4fcaaa
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2023-01-04 22:17:53 +0900

    feat: 문제
    Author: 박상준

--- lecture/codetree/시뮬레이션/격자 안에서 터지고 떨어지는 경우/1차원 폭발 게임.py
+++ lecture/codetree/시뮬레이션/격자 안에서 터지고 떨어지는 경우/1차원 폭발 게임.py
@@ -14,18 +14,29 @@ boom_list = [
     int(input())
     for _ in range(n)
 ]
-print(f"boom_list = {boom_list}")
 trigger = True
-while trigger:
-    tmp_boom = boom_list[:]
-    if len(boom_list):
-        first = boom_list[0]
-        cnt = 1
+
+def end_idx_return(start_idx, current_num):
+    for end_idx in range(start_idx + 1, len(boom_list)):
+        if boom_list[end_idx] != current_num:
+            return end_idx - 1
+    return len(boom_list) - 1
+
+while True:
+    did_explode = False
+    
+    for idx, value in enumerate(boom_list):
+        if value == 0:
+            continue
+        end_idx = end_idx_return(idx, value)
         
-        for i in range(1, len(boom_list)):
-            if first == boom_list[i]:
-                cnt += 1
-                if cnt >= m:
-                    pass
-    else:
+        if end_idx - idx + 1 >= m:
+            boom_list[idx:end_idx + 1] = [0] * (end_idx - idx + 1)
+            did_explode = True
+    boom_list = list(filter(lambda x: x > 0, boom_list))
+    
+    if not did_explode:
         break
+print(len(boom_list))
+for num in boom_list:
+    print(num)

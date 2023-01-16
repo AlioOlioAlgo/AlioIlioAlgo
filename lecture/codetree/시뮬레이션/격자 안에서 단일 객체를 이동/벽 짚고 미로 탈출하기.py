dab262fefc62928724eb8611e6ccd09f7564e15c
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-01-16 18:14:04 +0900

    feat: 상준

--- lecture/codetree/시뮬레이션/격자 안에서 단일 객체를 이동/벽 짚고 미로 탈출하기.py
+++ lecture/codetree/시뮬레이션/격자 안에서 단일 객체를 이동/벽 짚고 미로 탈출하기.py
@@ -15,4 +15,15 @@ graph = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
-# 오른손에 어떤 벽을 짚고 있는지 확인한다.
+
+# 그래프안에 위치한지 체크
+def in_graph(x, y):
+    return 0 < x <= n and 0 < y <= n
+
+# 바라보고 있는 방향으로 이동하는 것이 가능한 경우
+# 바로 앞이 격자 밖이라면 이동하여 탈출합니다.
+
+def avaliable_forward_out_graph(x, y):
+    pass
+#
+# 해당 방향으로 이동했다 가정시 해당 방향을 기준으로 오른쪽에 짚을 벽이 있다면, 그 방향으로 한칸 이동

23cb26e832fcdd86136f3f903eeaaf4ab4a985ac
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2023-01-25 19:25:36 +0900

    feat: 문제
    Author: 박상준

--- lecture/codetree/백트래킹/아름다운 수.py
+++ lecture/codetree/백트래킹/아름다운 수.py
@@ -9,7 +9,28 @@
  * -----------------------------------------------------------
  * 2023-01-25        ipeac       최초 생성
  """
-n = int(input())
+import sys
 
-def solution():
-    pass
+input = sys.stdin.readline
+n = int(input())
+global_cnt = 0
+ans = []
+
+def solution(cnt):
+    global global_cnt
+    if cnt == n:
+        global_cnt += 1
+        return
+    elif cnt > n:
+        return
+    else:
+        for i in range(1, 5):
+            for k in range(i):
+                ans.append(i)
+            solution(cnt + i)
+            
+            for k in range(i):
+                ans.pop()
+
+solution(0)
+print(global_cnt)

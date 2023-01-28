74e8277a1f918e299745c6a927e408e1177df687
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2023-01-28 20:24:48 +0900

    feat: 문제
    Author: 박상준

--- lecture/codetree/백트래킹/강력한 폭발.py
+++ lecture/codetree/백트래킹/강력한 폭발.py
@@ -26,11 +26,30 @@ def bomb(x, y, b_type):
     
     # 격자 내 칸에 대해서만 영역을 표시한다.
     for i in range(5):
-        dx, dy = bomb_shapes[b_type]
-    pass
+        dx, dy = bomb_shapes[b_type][i]
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny):
+            bombed[nx][ny] = 1
 
 def calc():
-    pass
+    # 폭탄이 터진 위치를 표시하는 배열을 초기화함
+    for i in range(n):
+        for j in range(n):
+            bombed[i][j] = 0
+    
+    # 각 폭탄의 타입에 따라 초토화 되는 영역을 표시함
+    for i in range(n):
+        for j in range(n):
+            if bomb_type[i][j]:
+                bomb(i, j, bomb_type[i][j])
+    
+    # 초토화된 영역의 수를 구함.
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if bombed[i][j]:
+                cnt += 1
+    return cnt
 
 def find_max_area(cnt):
     global ans
@@ -40,7 +59,9 @@ def find_max_area(cnt):
         return
     for i in range(1, 4):
         x, y = bomb_pos[cnt]
-        bomb_type[x][y]
+        bomb_type[x][y] = i
+        find_max_area(cnt + 1)
+        bomb_type[x][y] = 0
 
 for i in range(n):
     given_row = list(map(int, input().split()))
@@ -49,3 +70,5 @@ for i in range(n):
             bomb_pos.append((i, j))
 
 find_max_area(0)
+
+print(ans)

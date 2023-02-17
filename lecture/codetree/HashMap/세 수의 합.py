c5d9ed01b9f4b288cd29447d5920a3ceba2c3fd5
박상준 <psjoon@epart.com>
2023-02-17 20:54:13 +0900

    .

--- lecture/codetree/HashMap/세 수의 합.py
+++ lecture/codetree/HashMap/세 수의 합.py
@@ -9,3 +9,20 @@
  * -----------------------------------------------------------
  * 2023-02-17        ipeac       최초 생성
  """
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+dic = dict()
+
+for value in arr:
+    if value in dic:
+        dic[value] += 1
+    else:
+        dic[value] = 1
+ans = 0
+for i in range(n):
+    dic[arr[i]] -= 1
+    for j in range(i):
+        diff = k - arr[i] - arr[j]
+        if diff in dic:
+            ans += dic[diff]
+print(ans)

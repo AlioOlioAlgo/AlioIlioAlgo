b0c0d9f6c6fadac99caf35d84683945e26c2ba5c
박상준 <psjoon@epart.com>
2023-02-18 15:38:08 +0900

    .

--- lecture/codetree/HashMap/숫자 등장 횟수.py
+++ lecture/codetree/HashMap/숫자 등장 횟수.py
@@ -1,6 +1,6 @@
 """
  *packageName    : 
- * fileName       : 두 수의 합
+ * fileName       : 숫자 등장 횟수
  * author         : ipeac
  * date           : 2023-02-17
  * description    :
@@ -8,19 +8,17 @@
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2023-02-17        ipeac       최초 생성
- """
+"""
 from collections import defaultdict
 
-n, k = map(int, input().split())
+n, m = map(int, input().split())
 arr = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+print(f"arr  ==> {arr}")
+print(f"arr2  ==> {arr2}")
 count = defaultdict(int)
-
-ans = 0
-
-for i in range(n):
-    diff = k - arr[i]
-    
-    if diff in count:
-        ans += count[diff]
-    count[arr[i]] += 1
-print(ans)
+for ar in arr:
+    count[ar] += 1
+for value in arr2:
+    print(count[value], end=" ")

3e73b20a6eb0a17ae07b38a840ca6569af50c2f5
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-02-22 17:27:44 +0900

    feat: 상준

---
+++ lecture/codetree/TreeSet/top k 숫자.py
@@ -0,0 +1,12 @@
+"""
+ *packageName    : 
+ * fileName       : top k 숫자
+ * author         : ipeac
+ * date           : 2023-02-22
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-02-22        ipeac       최초 생성
+ """
+n, k = map(int, input().split())

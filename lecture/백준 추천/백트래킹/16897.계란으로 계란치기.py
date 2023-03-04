82452b932b6a0edde48bc9612437c3f013fec0d6
박상준 <psjoon@epart.com>
2023-03-04 23:08:53 +0900

    경사로

---
+++ lecture/백준 추천/백트래킹/16897.계란으로 계란치기.py
@@ -0,0 +1,51 @@
+"""
+ *packageName    : 
+ * fileName       : 16897.계란으로 계란치기
+ * author         : ipeac
+ * date           : 2023-03-04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-03-04        ipeac       최초 생성
+"""
+n = int(input())
+eggs = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+# print(f"eggs  ==> {eggs}")
+# print("======================================================")
+
+ans = -1e9
+
+def dfs(idx, eggs):
+    global ans
+    if idx == n - 1:
+        # print(sum([1 for i in eggs if i[0] <= 0]))
+        
+        ans = max(ans, sum([1 for i in eggs if i[0] <= 0]))
+        return
+    if eggs[idx][0] <= 0:  # 손에 들고 있는 계란이 깨진 경우 다음으로 ..
+        dfs(idx + 1, eggs)
+        return
+    
+    for i in range(n):
+        if i != idx and eggs[i][0] > 0:  # 나 자신이 아니고, 왼손에 든 계란이 내구도가 다 떨어진 계란이 아니여야합니다.
+            # print("======================================================")
+            eggs_copy = [i[:] for i in eggs]
+            # print(f"eggs  ==> {eggs}")
+            
+            eggs_copy[idx][0] -= eggs_copy[i][1]
+            eggs_copy[i][0] -= eggs_copy[idx][1]
+            
+            if eggs_copy[idx][0] <= 0:
+                eggs_copy[idx] = [0, 0]
+            if eggs_copy[i][0] <= 0:
+                eggs_copy[i] = [0, 0]
+            
+            # print(f"eggs_copy  ==> {eggs_copy}")
+            dfs(idx + 1, eggs_copy)
+
+dfs(0, eggs)
+print(ans)
---
+++ lecture/백준 추천/백트래킹/1941.소문난 칠공주.py
@@ -0,0 +1,51 @@
+"""
+ *packageName    : 
+ * fileName       : 1941.소문난 칠공주
+ * author         : ipeac
+ * date           : 2023-03-04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-03-04        ipeac       최초 생성
+"""
+from collections import deque
+from itertools import combinations
+
+graph = [
+    list(map(str, input()))
+    for _ in range(5)
+]
+dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
+
+def in_range(x, y):
+    return 0 <= x < 5 and 0 <= y < 5
+
+def connected(combi):
+    x, y = combi[0] // 5, combi[0] % 5
+    visited = [
+        [False for _ in range(5)]
+        for _ in range(5)
+    ]
+    visited[x][y] = True
+    
+    q = deque()
+    q.append((x, y))
+    cnt = 1
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if in_range(nx, ny) and (nx * 5 + ny) in combi and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny))
+                cnt += 1
+    return cnt
+
+ans = 0
+for combi in combinations(range(25), 7):
+    if sum(graph[i // 5][i % 5] == 'S' for i in combi) >= 4:  # 이다솜파가 4명이상이여합니다.
+        # 연결되어있는지 확인합니다.
+        if connected(combi) == 7:
+            ans += 1
+print(ans)

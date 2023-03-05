529cddcc2b1f84abc75f1460b142cdb5a28a5f4a
박상준 <psjoon@epart.com>
2023-03-05 23:58:39 +0900

    경사로

--- lecture/백준 추천/BFS/2573.빙산.py
+++ lecture/백준 추천/BFS/2573.빙산.py
@@ -9,3 +9,15 @@
  * -----------------------------------------------------------
  * 2023-03-05        ipeac       최초 생성
 """
+# 빙산이 두 덩어리 이상으로 분리되는 최초의 시간
+n, m = map(int, input().split())
+
+graph = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
+
+# 주변 빙산을 체크하는 함수
+def check_iceburg(x, y):
+    pass

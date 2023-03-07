58a085e885fc5017abe613cb1fdc0a7bf174d752
박상준 <psjoon@epart.com>
2023-03-08 01:19:06 +0900

    경사로

--- lecture/백준 추천/BFS/2206.벽 부수고 이동하기.py
+++ lecture/백준 추천/BFS/2206.벽 부수고 이동하기.py
@@ -17,31 +17,36 @@ graph = [
     list(map(int, input()))
     for _ in range(n)
 ]
-print(f"graph  ==> {graph}")
 
 dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
 
+ans = 1e9
+
 def bfs():
+    global ans
     visited = [
-        [False for _ in range(m)]
+        [[0] * 2 for _ in range(m)]
         for _ in range(n)
     ]
-    visited[0][0] = True
+    visited[0][0][0] = 1
     q = deque()
-    q.append((0, 0))
-    
+    q.append((0, 0, 0))
     while q:
-        x, y = q.popleft()
+        x, y, z = q.popleft()
+        if x == n - 1 and y == m - 1:
+            return visited[x][y][z]
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if in_range(nx,ny) and 
+            if in_range(nx, ny):
+                if graph[nx][ny] == 1 and z == 0:  # 벽이 있는 데 아직 벽을 부순적 없는 경우
+                    visited[nx][ny][1] = visited[x][y][0] + 1
+                    q.append((nx, ny, 1))
+                elif graph[nx][ny] == 0 and visited[nx][ny][z] == 0:  # 벽이 없는 경우
+                    visited[nx][ny][z] = visited[x][y][z] + 1
+                    q.append((nx, ny, z))
+    return -1
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            graph[i][j] = 0
-            bfs()
-            graph[i][j] = 1
+print(bfs())

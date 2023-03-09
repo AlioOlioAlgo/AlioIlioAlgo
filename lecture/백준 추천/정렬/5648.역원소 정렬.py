4be4d84df2431f1d2a8c6d91c04237778e36151d
박상준 <psjoon@epart.com>
2023-03-10 00:32:36 +0900

    경사로

--- lecture/백준 추천/정렬/5648.역원소 정렬.py
+++ lecture/백준 추천/정렬/5648.역원소 정렬.py
@@ -9,8 +9,24 @@
  * -----------------------------------------------------------
  * 2023-03-09        ipeac       최초 생성
 """
-arr = []
-n = 0
 
-while True:
+li = list()
+cnt = 0
+while 1:
+    try:
+        ins = input()
+        if ins == -1:
+            break
+        else:
+            i_sp = ins.split()
+            for i in i_sp:
+                if cnt != 0:
+                    i = "".join(reversed(i)).lstrip("0")
+                    li.append(int(i))
+                cnt += 1
+    except EOFError:
+        break
 
+res = sorted(li)
+for i in res:
+    print(i)

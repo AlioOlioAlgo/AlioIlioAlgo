7092e3ffb66c2eaa48e62738e6326f55b5992167
박상준 <psjoon@epart.com>
2023-03-14 00:19:52 +0900

    경사로

---
+++ lecture/백준 추천/시뮬레이션/13460.구슬 탈출 2.py
@@ -0,0 +1,16 @@
+"""
+ *packageName    : 
+ * fileName       : 13460.구슬 탈출 2
+ * author         : ipeac
+ * date           : 2023-03-14
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-03-14        ipeac       최초 생성
+"""
+n, m = map(int, input().split())
+graph = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]

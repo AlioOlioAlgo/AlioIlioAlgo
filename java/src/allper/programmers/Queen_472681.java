bb72382b061d3d20a2f936205744065a2f715bda
박상준 <psjoon@epart.com>
2023-03-21 02:12:32 +0900

    경사로

--- java/src/allper/programmers/Queen_472681.java
+++ java/src/allper/programmers/Queen_472681.java
@@ -17,23 +17,45 @@ public class Queen_472681 {
     static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     
-    static private
+    static int[] board;
+    static int answer;
     
-    static class Solution {
-        public int solution(int n) {
-            int answer = 0;
-            
-            return answer;
+    public static int solution(int n) {
+        board = new int[n];
+        
+        placeQueen(n, 0);
+        return answer;
+    }
+    
+    public static void placeQueen(int n, int row) {
+        if (row == n) {
+            answer++;
+            return;
         }
-        
-        public void placeQueen() {
-        
+        for (int i = 0; i < n; i++) {
+            board[row] = i;
+            if (isPossible(row)) {
+                placeQueen(n, row + 1);
+            }
         }
     }
     
+    private static boolean isPossible(int row) {
+        for (int i = 0; i < row; i++) {
+            // 같은 열일때 절대 안됨!
+            if (board[i] == board[row]) {
+                return false;
+            }
+            //대각선이 겹치는지 체크합니다
+            if (Math.abs(row - i) == Math.abs(board[row] - board[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     public static void main(String[] args) throws IOException {
-        Solution solution = new Solution();
-        bw.write(solution.solution(4) + "\n");
+        bw.write(solution(4) + "\n");
         bf.close();
         bw.flush();
         bw.close();

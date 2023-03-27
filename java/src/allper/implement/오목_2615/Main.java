ea6c935b0a68076db077413e272831d81031006e
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-03-27 23:18:07 +0900

    feat: ZOAC
    https://www.acmicpc.net/problem/16719
    
     집가서 한번더 확인해본다

--- java/src/allper/implement/오목_2615/Main.java
+++ java/src/allper/implement/오목_2615/Main.java
@@ -16,19 +16,68 @@ import java.util.StringTokenizer;
  * -----------------------------------------------------------
  * 2023-03-27        ipeac       최초 생성
  */
+class Pos {
+    int x;
+    int y;
+    
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Main {
     static int[][] graph;
+    static int[][] moves = {{0, 1}, {1, 1}, {1, 0}, {-1, 1}};
     
     public static void main(String[] args) throws IOException {
         FastReader rd = new FastReader();
         graph = new int[19][19];
         for (int i = 0; i < 19; i++) {
-            int[] line = new int[19];
-            for (int j = 0; j < 19; j++) {
-                line[i] = rd.nextInt();
+            String input = rd.nextLine();
+            for (int j = 0, index = 0; j < 19; index += 2, j++) {
+                graph[i][j] = Character.getNumericValue(input.charAt(index));
             }
-            graph[i] = line;
         }
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 19; j++) {
+                if (graph[i][j] != 0) {
+                    for (int k = 0; k < 4; k++) {
+                        if (lineDrive(i, j, k) == 5) {
+                            int[] dir = moves[k];
+                            //만약 5개 를 충족하였더라도 반대 방향에 해당 값과 동일한 값이 있으면 안됩니다.
+                            if (inRange(i - dir[0], j - dir[1]) && graph[i][j] == graph[i - dir[0]][j - dir[1]]) {
+                                continue;
+                            }
+                            if (graph[i][j] == 1 || graph[i][j] == 2) {
+                                System.out.println(graph[i][j]);
+                                System.out.println((i + 1) + " " + (j + 1));
+                                System.exit(0);
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(0);
+    }
+    
+    public static int lineDrive(int x, int y, int dir) {
+        int count = 1;
+        int[] moveD = moves[dir];
+        int nx = x + moveD[0], ny = y + moveD[1];
+        
+        while (inRange(nx, ny) && graph[nx][ny] == graph[x][y]) {
+            count++;
+            nx += moveD[0];
+            ny += moveD[1];
+        }
+        return count;
+    }
+    
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < 19 && 0 <= y && y < 19;
     }
     
     static class FastReader {
@@ -42,7 +91,7 @@ public class Main {
         String next() {
             while (st == null || !st.hasMoreElements()) {
                 try {
-                    st = new StringTokenizer(br.readLine());
+                    st = new StringTokenizer(br.readLine(), " ");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
